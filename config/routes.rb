be175d9217bf54b74d8720bec34fe41833281d0d
Daria Shcheglova <ellerden@gmail.com>
2019-02-21 14:16:55 +0300

    create all slim views for question

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  resources :questions
+  root 'questions#index'
+  resources :questions do 
+    resources :answers, shallow: true
+  end
 end
---
+++ spec/controllers/answers_controller_spec.rb
@@ -0,0 +1,6 @@
+require 'rails_helper'
+
+RSpec.describe AnswersController, type: :controller do
+  let(:answer) { create(:answer) }
+
+end
