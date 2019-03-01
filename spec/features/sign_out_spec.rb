c09d5772a7696998f86c6a8dd31f016e362966c8
Daria Shcheglova <ellerden@gmail.com>
2019-03-01 08:37:29 +0100

    acceptance test sign out

--- spec/features/sign_out_spec.rb
+++ spec/features/sign_out_spec.rb
@@ -6,26 +6,12 @@ feature 'Signed in user can sign out', %q{
 } do
   given(:user) { create(:user) }
 
-  background { visit destroy_user_session_path }
+  background do
+    sign_in(user)
+    click_on 'Log Out'
+  end
 
   scenario 'Authenticated user tries to sign out' do
-    # fill_in 'Email', with: user.email
-    # fill_in 'Password', with: user.password
-    # click_on 'Log in'
-    # save_and_open_page
     expect(page).to have_content 'Signed out successfully.'
-    # проверить это
-  end
-
-  scenario 'Unregistered user tries to sign in' do
-    # fill_in 'Email', with: 'wrong@test.com'
-    # fill_in 'Password', with: '12345678'
-    # click_on 'Log in'
-
-    expect(page).to have_content 'Invalid Email or password.'
   end
 end
-
-
- # destroy_user_session DELETE /users/sign_out(.:format) 
- #   devise/sessions#destroy
