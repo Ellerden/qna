01127fddc3d0c16ea3c9e368ea07471a99313bf8
Daria Shcheglova <ellerden@gmail.com>
2019-04-08 21:52:39 +0300

    all feature tests for adding votes to answers

--- spec/features/vote/add_vote_to_answer_spec.rb
+++ spec/features/vote/add_vote_to_answer_spec.rb
@@ -2,74 +2,75 @@ require 'rails_helper'
 
 feature "User can vote", %q{
   As a user
-  I'd like to be able to vote for question
+  I'd like to be able to vote for answer
   Which I find usefull or unuseful
 } do
   given(:user) { create(:user) }
   given(:user2) { create(:user) }
   given(:question) { create(:question, author: user) }
+  given!(:answer) { create(:answer, question: question, author: user) }
 
   describe 'Unauthenticated user', js: true do
     background { visit question_path(question) }
 
-    scenario 'tries to upvote a question' do
-      within '.question-vote' do
+    scenario 'tries to upvote an answer' do
+      within ".answer_#{answer.id} .answer-vote" do
         click_on '+'
         expect(page).to have_content '0'
       end
     end
 
-    scenario 'tries to downvote a question' do
-      within '.question-vote' do
+    scenario 'tries to downvote an answer' do
+      within ".answer_#{answer.id} .answer-vote" do
         click_on '-'
         expect(page).to have_content '0'
       end
     end
 
     scenario 'sees question score' do
-      within all('.question-vote.score') { expect(page).to have_content '0' }
+      within all(".answer_#{answer.id}.answer-vote.score") { expect(page).to have_content '0' }
     end
   end
 
   describe 'Authenticated user', js: true do  
-    context 'As NOT an author of the question'
+    context 'As NOT an author of the answer'
       background do 
         sign_in user2
         visit question_path(question)
       end
 
-      scenario 'upvotes someone elses question' do
-        within '.question-vote' do
+      scenario 'upvotes someone elses answer' do
+        within ".answer_#{answer.id} .answer-vote" do
           click_on '+'
           expect(page).to have_content '1'
         end
       end
 
       scenario 'cancels his/hers previous vote' do
-        within '.question-vote' do
+        within ".answer_#{answer.id} .answer-vote" do
           click_on '+'
           click_on '-'
           expect(page).to have_content '0'
         end
       end
 
-      scenario 'downvotes someone elses question' do
-        within '.question-vote' do
+      scenario 'downvotes someone elses answer' do
+        within ".answer_#{answer.id} .answer-vote" do
           click_on '-'
           expect(page).to have_content '-1'
         end
       end
 
-      scenario 'tries to upvote someone elses question twice' do
-        within '.question-vote' do
+      scenario 'tries to upvote someone elses answer twice' do
+        within ".answer_#{answer.id} .answer-vote" do
           click_on '+'
           click_on '+'
           expect(page).to have_content '1'
         end
       end
 
-      scenario 'tries to downvote someone elses question twice' do
-        within '.question-vote' do
+      scenario 'tries to downvote someone elses answer twice' do
+        within ".answer_#{answer.id} .answer-vote" do
           click_on '-'
           click_on '-'
           expect(page).to have_content '-1'
@@ -77,22 +78,22 @@ feature "User can vote", %q{
       end
     end
 
-    context 'As an author of the question' do
+    context 'As an author of the answer' do
       background do 
         sign_in user
         visit question_path(question)
       end
 
-      scenario 'tries to upvote his/her own question' do
-        within '.question-vote' do
+      scenario 'tries to upvote his/her own answer' do
+        within ".answer_#{answer.id} .answer-vote" do
           expect(page).not_to have_link '+'
         end
       end
 
-      scenario 'tries to downvote his/her own question' do
-        within '.question-vote' do
+      scenario 'tries to downvote his/her own answer' do
+        within ".answer_#{answer.id} .answer-vote" do
           expect(page).not_to have_link '-'
         end
       end
     end
-  end
+end
