e0de6463f4cf35f353660a9f07009ff0caa97f36
Daria Shcheglova <ellerden@gmail.com>
2019-07-03 15:11:56 +0300

    add config.action_mailer.default_url_option to production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -32,7 +32,7 @@ Rails.application.configure do
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
 
-  #config.action_mailer.default_url_options = { host: 'test-meister.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: '142.93.229.252' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address: 'smtp.gmail.com',
