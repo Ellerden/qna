5bc03842c047ad361fd1e7355c30a08b00e8992a
Daria Shcheglova <ellerden@gmail.com>
2019-07-04 19:36:07 +0300

    add sidekiq configuration for deploy

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,7 +1,8 @@
 # frozen_string_literal: true
 
 Rails.application.configure do
-  config.action_cable.allowed_request_origins = ["142.93.229.252"]
+  config.action_cable.allowed_request_origins = ["142.93.229.252", "qnaportal.site"]
+  set :sidekiq_config, -> { File.join(current_path, ‘config’, ‘sidekiq.yml’) }
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
